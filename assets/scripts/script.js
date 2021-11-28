0d015a6805630a485a14fe9654873d5565e3acbd
Mohamed-AR <88565049+Mohamed-AR@users.noreply.github.com>
2021-11-28 20:31:15 +0200

    Add files via upload

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+const lightbox = GLightbox({
+  href: "https://www.youtube.com/watch?v=nMR9Kyk6cs4",
+  type: "video",
+  source: "youtube", //vimeo, youtube or local
+  width: 900,
+  autoPlayVideos: "true",
+});
